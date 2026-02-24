d84b2ad41e392c20b4babc0e07b040c5ea8788bb
Nguyen Tan Loc <nguyenloc192005@gmail.com>
2026-02-24 21:41:31 +0700

    done

--- BT_2.js
+++ BT_2.js
@@ -1,7 +1,13 @@
 const numOfPuschar = 5;
 let total = 0 ;
 for( let i = 0 ; i < 5 ; i ++){
-    let price  = Math.round(Math.random()*5000000);
+    let discount = Math.round(Math.random()*1);
+    if(discount === 1){
+       let discountPercent = Math.round(Math.random()*50);
+       total += (discountPercent/100)*Math.round(Math.random()*5000000);
+    } else {
+        let price  = Math.round(Math.random()*5000000);
     total += price;
+    }
 }
 console.log(`tong la :${total}`);
