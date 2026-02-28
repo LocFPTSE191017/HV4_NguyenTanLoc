3aef5da1c5af78debb88b43404ad3c1c363b600a
Nguyen Tan Loc <nguyenloc192005@gmail.com>
2026-02-28 21:34:52 +0700

    done

--- Array2.js
+++ Array2.js
@@ -2,7 +2,7 @@ const friends = ["Kiet","Hieu","Vinh"];
 friends.push("Khanh");
 console.log(`Number of friends: ${friends.length}`);
 console.log(`First friend: ${friends[0]}`);
-friends.shift("Tuan");
+friends.unshift("Tuan");
 friends.pop();
 friends.splice(2,1,"Phuc");
 for(let i = 0; i < friends.length; i++){
