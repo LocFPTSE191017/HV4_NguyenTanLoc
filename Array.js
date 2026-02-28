58de4919165ec9b4785ad820e0882b5c87eae4ad
Nguyen Tan Loc <nguyenloc192005@gmail.com>
2026-02-28 20:11:48 +0700

    Array Example

---
+++ Array.js
@@ -0,0 +1,4 @@
+const carts = ["hang1", "hang2", "hang3", "hang4", "hang5", "hang6", "hang7", "hang8", "hang9", "hang10"];
+console.log(`so luong san pham trong gio hang la: ${carts.length}`);
+console.log(`san pham dau tien : ${carts[0]}`);
+console.log(`san pham cuoi cung : ${carts[carts.length - 1]}`);
