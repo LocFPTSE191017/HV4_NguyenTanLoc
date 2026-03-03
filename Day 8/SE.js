4ddf42513b0c8738fcf7e66bd321c26c0d17dfa6
Nguyen Tan Loc <nguyenloc192005@gmail.com>
2026-03-03 21:17:17 +0700

    day 8

--- Day 8/SE.js
+++ Day 8/SE.js
@@ -1,7 +1,14 @@
-const Arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const tb = Arr.filter((num) => {
-    return num >= 5;
-});
-console.log(tb);
-const pass = Arr.every((num) => num >= 5);
-console.log(pass);
+const scores = [6, 7, 8, 4, 9, 5, 7, 6, 8, 10];
+
+const hasFail = scores.some(score => score < 5);
+if(hasFail){
+    console.log("Có bạn dưới TB");
+} else{
+    console.log("Không có bạn nào dưới TB");
+}
+const allPass = scores.every(score => score >= 5);
+if(allPass){
+    console.log("Cả lớp qua môn");
+} else{
+    console.log("Có bạn chưa qua môn");
+}   
